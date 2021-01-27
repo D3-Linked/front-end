f103c2a10c1016469ed3e6e957f3cf23cfc7723a
Sam Van den Bergh <r0751478@users.noreply.github.com>
2021-01-27 11:17:29 +0100

    home voor luca

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './user/home/home.component';
 import { TableComponent } from './user/table/table.component';
 
 
 const routes: Routes = [
+  {path: '', component: HomeComponent},
   {path: 'table', component: TableComponent}
 ];
 
