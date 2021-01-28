bc542ca6f003f81364603f6d0cd3007bd80c431d
Sam Van den Bergh <r0751478@users.noreply.github.com>
2021-01-28 10:37:46 +0100

    planning

--- src/app/user/home/home.component.ts
+++ src/app/user/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
@@ -6,12 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  panelOpenState = false;
+  public code: number;
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
   }
-
+  goTo(id: number) {
+    this.route.navigate(['/userlevering'], { queryParams: { id }});
+  }
+  
 }
 
 
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,15 +4,17 @@ import { TableComponent } from './table/table.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { EigenPlanningComponent } from './eigen-planning/eigen-planning.component';
 
 
 const appRoutes: Routes = [
   { path: 'table', component: TableComponent },
+  { path: 'userlevering', component: EigenPlanningComponent },
 
 ];
 
 @NgModule({
-  declarations: [TableComponent, HomeComponent],
+  declarations: [TableComponent, HomeComponent, EigenPlanningComponent],
   imports: [
     CommonModule,
     RouterModule.forRoot(appRoutes, { enableTracing: true }),
